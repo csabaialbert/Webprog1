a7b6a567b623243469694ab98021d5cd38b4cc3d
Regéczy Ákos <regeczya@foketkp.local>
2022-04-29 01:55:23 +0200

    Update kapcsolat.tpl.php

--- Beadando/Weboldal/templates/pages/kapcsolat.tpl.php
+++ Beadando/Weboldal/templates/pages/kapcsolat.tpl.php
@@ -35,6 +35,17 @@
             <p><b>Telefonszámunk 10 – 18 óra között hívható.</b> A telefonon Önkéntesünk fogadja a hívásokat, ahogy ideje engedi, ezért megértésüket kérjük, ha esetenként a szám nem elérhető. Kérjük, legyenek türelemmel és ilyen esetben próbálják kicsit később hívni! Visszahívást kezdeményezni nem áll módunkban.
             </p>
         </div>
-        
-    
+        <h1>Kapcsolat</h1>
+        <form name="kapcsolat" action="php/kapcsolat.php" onsubmit="return ellenoriz();" method="post">
+            <div id="urlap">
+                <label><input type="text" id="nev" name="nev" size="20" maxlength="40">Név (minimum 5 karakter): </label>
+                <br/>
+                <label><input type="text" id="email" name="email" size="30" maxlength="40">E-mail (kötelező): </label>
+                <br/>
+                <label> <textarea id="szoveg" name="szoveg" cols="40" rows="10"></textarea> Üzenet (kötelező): </label>
+                <br/>
+                <input id="kuld" type="submit" value="Küld">
+                <button onclick="ellenoriz();" type="button">Ellenőriz</button>         
+            </div>
+        </form>
 </body>
